2e9eb1b2050503f7b329ee8d795c02a442032299
adityac2410 <107806841+adityac2410@users.noreply.github.com>
2022-07-22 19:25:17 +0530

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import Header from "./Components/Header";
+import Slidebar from "./Components/Slidebar";
+import Form from "./Form";
+import { Route, Switch } from "react-router-dom";
+// import Logo1 from './Images/Logo1.png'
+// import Logo2 from './Images/Logo2.png'
+import "./index.css";
+
+export default function Project() {
+  return (<>
+    <Header />
+    <Slidebar />   
+    <Form />
+    </>
+  );
+}
+
