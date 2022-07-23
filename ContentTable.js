96d4541f9b6f5cbc1ed3d5b3b48b14763ddfc468
adityac2410 <107806841+adityac2410@users.noreply.github.com>
2022-07-23 09:03:32 +0530

    Add files via upload

--- ContentTable.js
+++ ContentTable.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useEffect,useState } from "react";
 import SearchIcon from '../Images/SearchIcon.svg';
 import LeftArrow from  '../Images/LeftArrow.svg';
 import RightArrow from '../Images/RightArrow.svg';
 
-const data = [
-  { SrNo: "1",Project:"Scrobits Dev", ServerAddress:"scrobitsDev.com", SSLExp:"in 2d",Lastupdated:"5 mins ago", Resptime:"500ms",Status:"Healthy",Details:"Link"},
+// const data1 = [
+//   { SrNo,Project, ServerAddress, SSLExp,
+//   Lastupdated, Resptime,Status,Details},
+// ]
+
+const data1 = [
+  { SrNo: "1",Project:"Scrobits Dev", ServerAddress:"scrobitsDev.com", SSLExp:"in 2d",
+  Lastupdated:"5 mins ago", Resptime:"500ms",Status:"Healthy",Details:"Link"},
 ]
 
+ function serverStatus(){
+    //  const [data1,setServer] = useState([]);
+   
+   const url="https://api-monitor.scrobits.com/api/v1/users/projects/8";
+    fetch(url).then((response)=>{
+      return response.json();
+    }).then((data)=>{
+      let serverData= data;
+      console.log(serverData);
+    })
+}
+
 export default function ContentTable() {
+
   return (
-    <div className='ContentTable'>
+    <div className='ContentTable' onLoad={(serverStatus())}>
      <div className="Container">
      <img className='SearchIcon' src={SearchIcon}/>
         <input type="text" placeholder="Search project " className="Search"></input>
      </div>
         
       <table className='Table'>
+        <thead>
         <tr>
           <th className='columnName'><center>Sr.No</center></th>
           <th className='columnName'><center>Project</center></th>
@@ -26,11 +46,13 @@ export default function ContentTable() {
           <th className='columnName'><center>Status</center></th>
           <th className='columnName'><center>Details</center></th>
         </tr>
-        {data.map((val, key) => {
+        </thead>
+        {data1.map((val, key) => {
           return (
            <>
+           <tbody>
            <tr key={key}>
-           <td><center>{val.SrNo}</center></td>
+           <td><center>1</center></td>
               <td><center>{val.Project}</center></td>
               <td><center>{val.ServerAddress}</center></td>
               <td><center>{val.SSLExp}</center></td>
@@ -40,7 +62,7 @@ export default function ContentTable() {
               <td><center>{val.Details}</center></td>
             </tr>
             <tr className="EvenRow" key={key}>
-            <td><center>{val.SrNo}</center></td>
+            <td><center>2</center></td>
               <td><center>{val.Project}</center></td>
               <td><center>{val.ServerAddress}</center></td>
               <td><center>{val.SSLExp}</center></td>
@@ -50,7 +72,7 @@ export default function ContentTable() {
               <td><center>{val.Details}</center></td>
             </tr>
             <tr key={key}>
-            <td><center>{val.SrNo}</center></td>
+            <td><center>3</center></td>
               <td><center>{val.Project}</center></td>
               <td><center>{val.ServerAddress}</center></td>
               <td><center>{val.SSLExp}</center></td>
@@ -60,7 +82,7 @@ export default function ContentTable() {
               <td><center>{val.Details}</center></td>
             </tr>
             <tr className="EvenRow" key={key}>
-            <td><center>{val.SrNo}</center></td>
+            <td><center>4</center></td>
               <td><center>{val.Project}</center></td>
               <td><center>{val.ServerAddress}</center></td>
               <td><center>{val.SSLExp}</center></td>
@@ -70,7 +92,7 @@ export default function ContentTable() {
               <td><center>{val.Details}</center></td>
             </tr>
             <tr key={key}>
-            <td><center>{val.SrNo}</center></td>
+            <td><center>5</center></td>
               <td><center>{val.Project}</center></td>
               <td><center>{val.ServerAddress}</center></td>
               <td><center>{val.SSLExp}</center></td>
@@ -80,7 +102,7 @@ export default function ContentTable() {
               <td><center>{val.Details}</center></td>
             </tr>
             <tr className="EvenRow" key={key}>
-            <td><center>{val.SrNo}</center></td>
+            <td><center>6</center></td>
               <td><center>{val.Project}</center></td>
               <td><center>{val.ServerAddress}</center></td>
               <td><center>{val.SSLExp}</center></td>
@@ -90,7 +112,7 @@ export default function ContentTable() {
               <td><center>{val.Details}</center></td>
             </tr>
             <tr key={key}>
-            <td><center>{val.SrNo}</center></td>
+            <td><center>7</center></td>
               <td><center>{val.Project}</center></td>
               <td><center>{val.ServerAddress}</center></td>
               <td><center>{val.SSLExp}</center></td>
@@ -100,7 +122,7 @@ export default function ContentTable() {
               <td><center>{val.Details}</center></td>
             </tr>
             <tr className="EvenRow" key={key}>
-            <td><center>{val.SrNo}</center></td>
+            <td><center>8</center></td>
               <td><center>{val.Project}</center></td>
               <td><center>{val.ServerAddress}</center></td>
               <td><center>{val.SSLExp}</center></td>
@@ -115,6 +137,7 @@ export default function ContentTable() {
               <center><div id='PageOutOf'> page out of </div></center> 
               <button type="button" id="RightButton"> <img src={RightArrow}></img></button>
             </tr>
+            </tbody>
           </> 
           )
         })}
@@ -122,3 +145,4 @@ export default function ContentTable() {
      </div>
   )
 }
+
